4c6e466ed6c6b1d82da99dc8efea6160854b69d0
nico604 <nicolas.girard.qc@gmail.com>
2020-03-27 13:41:22 -0400

    essaie

--- src/app/pays/pays.component.ts
+++ src/app/pays/pays.component.ts
@@ -3,11 +3,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { MessageService } from '../message.service';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-
 import { latLng, LatLng, tileLayer } from 'leaflet';
+
 @Component({
   selector: 'app-pays',
-  templateUrl: './pays.component.html',
+  templateUrl: './paypors.component.html',
   styleUrls: ['./pays.component.css']
 })
 
@@ -15,20 +15,27 @@ export class PaysComponent implements OnInit {
 
   name: string;
   coord: [number, number];
+  coord = [ 39.8282, -98.5795 ]; 
 
   options: any = {
     layers: [
-      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' })
+      tileLayer(
+        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+        { 
+          maxZoom: 18, 
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' 
+        }
+      )
     ],
     zoom: 5,
     center: latLng(this.coord)
   };
 
   constructor(
-  	private route: ActivatedRoute,
+  	private    route: ActivatedRoute,
 	  private location: Location,
     private enAction: MessageService,
-    private router: Router
+    private   router: Router
 	) { }
 
   ngOnInit() {
@@ -37,21 +44,22 @@ export class PaysComponent implements OnInit {
   }
   
   salutPays() {
+
   	this.route.paramMap.subscribe(params => {
       this.name = params.get('name');
-      if(this.name == "Bermudes"){
+      if ( this.name == "Bermudes"){
         console.log('Bermuded');
         this.coord = [46.879966,-121.726909];
-      }if(this.name == "Canada"){
+      } if (this.name == "Canada") {
         console.log('Canaded');
         this.coord = [ 39.8282, -98.5795 ]; 
       }
     })
+  
   }
   
   gotoHeroes() {
     this.router.navigate(['/']);
   }
-
 
 }
